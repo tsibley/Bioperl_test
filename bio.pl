ed60f9c8958a8c7a4b7c920639196828c5d8a3a7
Thomas Sibley <trsibley@uw.edu>
2014-03-25 14:43:03 -0700

    Enable strict and warnings to make code easier to maintain and correctly write

--- bio.pl
+++ bio.pl
@@ -1,11 +1,13 @@
 #!/Users/travis/perl5/perlbrew/perls/perl-5.18.2/bin/perl
+use strict;
+use warnings;
 
-$format1 = shift;
-$format2 = shift;
+my $format1 = shift;
+my $format2 = shift;
 
 use Bio::AlignIO;
 
-$in = Bio::AlignIO->newFh('-format' => $format1 );
-$out = Bio::AlignIO->newFh('-format' => $format2 );
+my $in = Bio::AlignIO->newFh('-format' => $format1 );
+my $out = Bio::AlignIO->newFh('-format' => $format2 );
 
 print $out $_ while <$in>;
